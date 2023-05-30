d1ec54c7b074f2857f04ab835b18da5a4a58c7b1
Alexey Shadow <alexey.bakushev@gemforge.ru>
2023-05-30 23:09:19 +0300

    'test'

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,19 +1,71 @@
-import "./styles.css";
+import Icon from "./Icon";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-import { useState } from "react";
-import { Reorder } from "framer-motion";
-import { Item } from "./Item";
+const style = {
+  width: "300px",
+  display: "flex",
+  borderRadius: "8px",
+  padding: "1rem",
+  border: "1px solid black",
+  alignItems: "center",
+  gap: "3rem",
+  fontSize: "24px",
+};
 
-const initialItems = ["🍅 Tomato", "🥒 Cucumber", "🧀 Cheese", "🥬 Lettuce"];
+const list = [
+  {
+    id: 1,
+    title: "1. Read books",
+  },
+  {
+    id: 2,
+    title: "2.  Go to work",
+  },
+  {
+    id: 3,
+    title: "3. Go to home",
+  },
+  {
+    id: 4,
+    title: "4. Eat mellon",
+  },
+  {
+    id: 5,
+    title: "5. Drink coffee",
+  },
+];
 
 export default function App() {
-  const [items, setItems] = useState(initialItems);
-
   return (
-    <Reorder.Group axis="y" onReorder={setItems} values={items}>
-      {items.map((item) => (
-        <Item key={item} item={item} />
-      ))}
-    </Reorder.Group>
+    <div>
+      <DragDropContext onDragEnd={(...props) => console.log(props)}>
+        <Droppable droppableId="droppable-1">
+          {(provided, _) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {list.map((item, i) => {
+                return (
+                  <Draggable
+                    key={item.id}
+                    draggableId={"draggable-" + item.id}
+                    index={i}
+                  >
+                    {(provided, _) => (
+                      <div
+                        style={style}
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                      >
+                        <Icon {...provided.dragHandleProps} />
+                        <span>{item.title}</span>
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
   );
 }
